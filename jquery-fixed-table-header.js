146697e6d8f08d9ba1ced9f881af2ac9e8cf09a7
zxy1994 <13510460187@163.com>
2017-07-17 16:43:10 +0800

    To solve sometime '$' invalid

--- jquery-fixed-table-header.js
+++ jquery-fixed-table-header.js
@@ -1,22 +1,24 @@
-$.fn.fixedTableHeader = function () {
-    var t = $(this);
-
-    var tableOffset = t.offset().top;
-    var fixed_table = $('<table></table>').css({ 'display':'none', 'position':'fixed', 'top':'0px', 'background-color':'white' });
-    t.parent().append(fixed_table.append(t.find("thead").clone()));
-
-    fixed_table.find("th").each(function (i) {
-        $(this).width(t.find("th").eq(i).width() + 1);
-    });
-
-    $(window).bind("scroll", function () {
-        var offset = $(this).scrollTop();
-        if (offset >= tableOffset && fixed_table.is(":hidden")) {
-            fixed_table.show();
-        }
-        else if (offset < tableOffset) {
-            fixed_table.hide();
-        }
-    });
-    return t;
-}
+(function($){
+    $.fn.fixedTableHeader = function () {
+	    var t = $(this);
+	
+	    var tableOffset = t.offset().top;
+	    var fixed_table = $('<table></table>').css({ 'display':'none', 'position':'fixed', 'top':'0px', 'background-color':'white' });
+	    t.parent().append(fixed_table.append(t.find("thead").clone()));
+	
+	    fixed_table.find("th").each(function (i) {
+	        $(this).width(t.find("th").eq(i).width() + 1);
+	    });
+	
+	    $(window).bind("scroll", function () {
+	        var offset = $(this).scrollTop();
+	        if (offset >= tableOffset && fixed_table.is(":hidden")) {
+	            fixed_table.show();
+	        }
+	        else if (offset < tableOffset) {
+	            fixed_table.hide();
+	        }
+	    });
+	    return t;
+	}
+}(jQuery));
