fc80df31e78f17cebbf5a85b5d8419a4daf5ac9e
zxy1994 <13510460187@163.com>
2017-07-18 15:48:29 +0800

    update

--- jquery-fixed-table-header.js
+++ jquery-fixed-table-header.js
@@ -1,13 +1,23 @@
 (function($){
     $.fn.fixedTableHeader = function () {
 	    var t = $(this);
-	
 	    var tableOffset = t.offset().top;
 	    var fixed_table = $('<table></table>').css({ 'display':'none', 'position':'fixed', 'top':'0px', 'background-color':'white' });
 	    t.parent().append(fixed_table.append(t.find("thead").clone()));
-	
+		
+		fixed_table.attr("border",t.attr("border"));
+		fixed_table.css("border-collapse",t.css("border-collapse"));
+		fixed_table.attr("cellspacing",t.attr("cellspacing"));
+
+		fixed_table.width(t.outerWidth());
+		
+		if(t.css("border-collapse")=="collapse"){
+			var borderWith = parseInt(t.css("border"));
+		}else{
+			var borderWith = 0;
+		}
 	    fixed_table.find("th").each(function (i) {
-	        $(this).width(t.find("th").eq(i).width() + 1);
+	        $(this).width(t.find("th").eq(i).width() + borderWith);
 	    });
 	
 	    $(window).bind("scroll", function () {
