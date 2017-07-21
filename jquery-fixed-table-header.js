df799031cce48908a7fb078e12969066cd232810
zxy1994 <13510460187@163.com>
2017-07-21 16:05:27 +0800

    update

--- jquery-fixed-table-header.js
+++ jquery-fixed-table-header.js
@@ -1,23 +1,33 @@
 (function($){
+	/**
+	 * 表头悬浮插件
+	 * @date 2017-07-18 13:17:54
+	 */
     $.fn.fixedTableHeader = function () {
 	    var t = $(this);
 	    var tableOffset = t.offset().top;
+	    // 创建一个表格，设置样式
 	    var fixed_table = $('<table></table>').css({ 'display':'none', 'position':'fixed', 'top':'0px', 'background-color':'white' });
+	   	// 把标题的标题clone过来
 	    t.parent().append(fixed_table.append(t.find("thead").clone()));
 		
+		// 设置表格属性和样式
 		fixed_table.attr("border",t.attr("border"));
 		fixed_table.css("border-collapse",t.css("border-collapse"));
 		fixed_table.attr("cellspacing",t.attr("cellspacing"));
-
+		// 设置表格的宽度
 		fixed_table.width(t.outerWidth());
 		
+		// 用于判断宽度该加多少
 		if(t.css("border-collapse")=="collapse"){
-			var borderWith = parseInt(t.css("border"));
+			var borderWith = parseInt(t.attr("border"));
 		}else{
 			var borderWith = 0;
 		}
+		// 设置表格悬浮标题单元格的宽度
 	    fixed_table.find("th").each(function (i) {
-	        $(this).width(t.find("th").eq(i).width() + borderWith);
+	    	var width = t.find("th").eq(i).css("width");
+	        $(this).width(parseInt(width)+borderWith+"px");
 	    });
 	
 	    $(window).bind("scroll", function () {
@@ -32,3 +42,4 @@
 	    return t;
 	}
 }(jQuery));
+
